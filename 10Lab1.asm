bdbee181b41553f88e75ffb026b575e8d2c9a66c
qw <ikherty@gmail.com>
2019-05-16 23:33:08 +0300

    dont work

--- 10Lab1.asm
+++ 10Lab1.asm
@@ -1,24 +1,28 @@
-; test
-; fasm demonstration of writing simple ELF executable
+; Используя только операцию OUTCH, -Макрокоманда вывода символа на экран
+; вывести содержимое регистра AX 
+; в виде беззнакового 16-ричного числа («буквенные» цифры — от A до F).
 
-format ELF executable 3
-entry start
+HexTabl  db     '0123456789abcdef'
+asHex    db     '00', '$'
 
-segment readable executable
-
-start:
-
-	mov	eax,4
-	mov	ebx,1
-	mov	ecx,msg
-	mov	edx,msg_size
-	int	0x80
-
-	mov	eax,1
-	xor	ebx,ebx
-	int	0x80
-
-segment readable writeable
-
-msg db 'Hello world!',0xA
-msg_size = $-msg
+;mov di, OFFSET asHex; запишет адрес конца строки
+lea di, asHex;осуществляет загрузку в регистр адреса
+@@Repeat: ;начало цикла
+mov cx, ax 
+and ax, 000fh; логическое умножение всех пар бит операндов
+;mov bx, OFFSET HexTabl 
+lea bx, HexTabl
+;xlat ; применяется для перекодирования байта по таблице
+;вывод с outch
+mov [di+1], al 
+mov ax, cx 
+and ax, 00f0h 
+mov cl, 4 
+shr ax, cl ; логический сдвиг вправо на сл бит
+;xlat 
+mov [di], al 
+mov ah, 09h 
+jnc    @@Repeat  ;конец цикла
+;mov dx, OFFSET asHex ; запишет адрес начала строки 
+lea dx, asHex
+int 21h 
