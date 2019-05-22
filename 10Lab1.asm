999bfbf5f4074eda82d65a539229f9384290e877
qw <ikherty@gmail.com>
2019-05-22 08:27:48 +0300

    some changes

--- 10Lab1.asm
+++ 10Lab1.asm
@@ -4,40 +4,36 @@
 
 format ELF64 executable 3
 entry start
-;include 'win64a.inc'
 
 segment readable executable
 start:
-
-;////////////////////////////// Тут происходит вся магия
-	mov rbx, table; указываю где неходится таблица для xlatb
-	mov rdi, temp+3; указываю куда записывать 16-ные значения
+	mov RBX, table; регистр, где неходится таблица для xlatb
+	mov RDI, temp+3; регистр для записи 16-ных значений
 	std; запись будет происходить в обратную сторону (от больших адресов к меньшим)
-	mov rax, [number]; запихиваю число в RAX
-	and rax, 0xffff; оставляю только последние 4 байта
-	mov rbp, rax; теперь это число будет храниться в RBP
+	mov RAX, [number]; помещаю число в RAX
+	and RAX, 0xffff; оставляю только последние 4 байта
+	mov RBP, RAX; теперь это число будет храниться в RBP
  .loop1:
-	mov rax, rbp; Считываю число
-	shr rbp, 4; сдвигаю его вправо на 4 бита
-	and rax, 0xf; очищаю все биты кроме первых 4-х (значение в AL является индексом в таблице по адресу в RBX. см. строку 13)
-	xlatb; Достаю байт из таблицы
+	mov RAX, RBP; считываю число
+	shr RBP, 4; сдвигаю его вправо на 4 бита
+	and RAX, 0xf; очищаю все биты кроме первых 4-х (значение в AL является индексом в таблице по адресу в RBX. см. строку 13)
+	xlatb; достаю байт из таблицы
 	stosb; сохраняю его в буффер
-	test rbp,rbp; Если я обработал не все, то начать заново
+	test RBP,RBP; если обработалось не все, то начать заново
 	jne .loop1; начать цикл заново
-;//////////////////////////////
 .exit:
 	;PRINT
-	mov edx, 5 ; string lenth
-	mov rsi, temp
-	mov edi, 1
-	mov eax, 1
+	mov EDX, 5 ; string lenth
+	mov RSI, temp
+	mov EDI, 1
+	mov EAX, 1
 	syscall ; WriteConsole
 
-	xor edi, edi ; Exit
-	mov eax, 60
+	xor EDI, EDI ; Exit
+	mov EAX, 60
 	syscall
 
 segment readable writable
 	number dq 0xdeadbeef; то самое число которе мы хотим представить в 16-м виде
-	temp db '0','0','0','0',10; Инициализирую строку нулями, чтобы числа < 0x1000 отображались корректно
-	table db '0123456789ABCDEF'; таблица символов, которую будет использовать XLATB
+	temp db '0','0','0','0',10; инициализирую строку нулями, чтобы числа < 0x1000 отображались корректно
+	table db '0123456789ABCDEF'; таблица символов, которую будем использовать XLATB
